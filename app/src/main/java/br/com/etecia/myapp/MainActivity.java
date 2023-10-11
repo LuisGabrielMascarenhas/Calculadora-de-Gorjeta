cdc941096c184be2ad18989565c74f14eff9162e
LuisGabrielMascarenhas <luis.gabriel.santos614@gmail.com>
2023-10-10 21:34:30 -0300

    Declarando variaveis na Main

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -3,12 +3,31 @@ package br.com.etecia.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+ TextInputLayout txtFieldValor,txtFieldValorGorjeta,txtFieldValorTotal;
+ Spinner qualidade_spinner;
+
+ Button btnCalcular;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtFieldValor = findViewById(R.id.txtFieldValor);
+        txtFieldValorGorjeta = findViewById(R.id.txtFieldValorGorjeta);
+        txtFieldValorTotal = findViewById(R.id.txtFieldValorTotal);
+
+        qualidade_spinner = findViewById(R.id.qualidade_spinner);
+        btnCalcular = findViewById(R.id.btnCalcular);
+
     }
 }
